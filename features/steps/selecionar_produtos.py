f5efa6b1922b8eeb9f0233f80b4533a119c13ad1
Lk71807 <lk-98@hotmail.com>
2024-03-29 23:17:36 -0300

    Scenario Outline  Solução sem  Hífen

--- features/steps/selecionar_produtos.py
+++ features/steps/selecionar_produtos.py
@@ -14,11 +14,32 @@ def step_impl(context):
     # Passo em Si
     context.driver.get("https://www.saucedemo.com")  # abrir navegador no endereço do site alvo
 
-
+# Preencher com usuário e senha
 @when(u'preencho os campos de login com usuario {usuario} e senha {senha}')
 def step_impl(context, usuario, senha):
      context.driver.find_element(By.ID, "user-name").send_keys(usuario)   # preencher o usuario
      context.driver.find_element(By.ID, "password").send_keys(senha)      # preencher senha
+     context.driver.find_element(By.ID, "login-button").click()           # clicar no botão login
+
+# Preencher com usuário em branco e senha
+@when(u'preencho os campos de login com usuario  e senha {senha}')
+def step_impl(context, senha):
+     # não preenche o usuário
+     context.driver.find_element(By.ID, "password").send_keys(senha)      # preencher senha
+     context.driver.find_element(By.ID, "login-button").click()           # clicar no botão login
+
+# Preencher com usuário, mas deixar a senha em branco
+@when(u'preencho os campos de login com usuario {usuario} e senha ')
+def step_impl(context, usuario):
+     context.driver.find_element(By.ID, "user-name").send_keys(usuario)   # preencher o usuario
+  # Não preencho a senha
+     context.driver.find_element(By.ID, "login-button").click()           # clicar no botão login
+
+# Clica no botão login sem ter preenchido o usuário e a senha
+@when(u'preencho os campos de login com usuario  e senha ')
+def step_impl(context):
+   # não preencho o usuário
+   # não preencho a senha
      context.driver.find_element(By.ID, "login-button").click()           # clicar no botão login
 
 
@@ -29,9 +50,7 @@ def step_impl(context):
  
  # teardowmn / encerramento
      context.driver.quit() 
-
-
-                                                                                                                                                         
+                                                                                                                                                       
 @then(u'exibe a mensagem de erro no login')
 def step_impl(context):
      # validar mensagem de erro
@@ -39,4 +58,12 @@ def step_impl(context):
 
      # teardown / encerramento
      context.driver.quit()
-     
+
+# Verifica a mensagem para o Scenario Outline
+@then(u'exibe a {mensagem} de erro no login')
+def step_impl(context, mensagem):
+     # validar mensagem de erro
+     assert context.driver.find_element(By.CSS_SELECTOR, "h3").text == mensagem
+
+     # teardown / encerramento
+     context.driver.quit()
